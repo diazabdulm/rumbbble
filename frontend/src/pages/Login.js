bde022630458380d3c414f5fd984b063bd9260b0
Abdul <abdul.diaz@fcb.com>
2020-05-11 07:17:32 -0400

    built out first few pieces of sign in page

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,5 +1,45 @@
 import React from "react";
+import styled from "styled-components";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Image from "react-bootstrap/Image";
+
+const Sidebar = styled(Jumbotron)`
+  background: #f1cdd7;
+  color: #865c6c;
+`;
+
+const LoginContainer = styled.div`
+  width: 100%;
+  max-width: 415px;
+  margin: auto;
+`;
 
 export default function Login() {
-  return <a href="/auth/login">Log in</a>;
+  return (
+    <Container fluid>
+      <Row>
+        <Sidebar className="d-none d-lg-block d-xl-block col-lg-5 vh-100 mb-0 rounded-0">
+          <Container>
+            <h3 className="font-weight-bold">
+              Discover the world's top
+              <br />
+              Developers & Creators
+            </h3>
+          </Container>
+        </Sidebar>
+        <div className="col-lg-7 vh-100 d-flex">
+          <LoginContainer className="">
+            <Image src={require("../assets/logo.png")} width="125" className="mb-5" />
+            <h3 className="mb-4 font-weight-bold">Sign in to Rumbbble</h3>
+            <Button variant="dark" block>
+              Sign in with Github
+            </Button>
+          </LoginContainer>
+        </div>
+      </Row>
+    </Container>
+  );
 }
