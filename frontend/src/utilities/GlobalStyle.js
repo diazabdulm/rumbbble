9202e8551abb6b748fc64da80be0225ed27473f6
Abdul <diazabdulm@gmail.com>
2020-06-03 15:39:47 -0400

    replaced Inter with a system font stack

--- frontend/src/utilities/GlobalStyle.js
+++ frontend/src/utilities/GlobalStyle.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
   body {
-    font-family: 'Inter', sans-serif;
+    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol";
     color: #0d0c22;
   }
 
